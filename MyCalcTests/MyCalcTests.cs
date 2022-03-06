98e2d67284f3b54e7a478ce7ae1fa0930ab36500
Svetlana <podsvetser@yandex.ru>
2022-03-07 00:47:26 +0300

    Initial commit

---
+++ MyCalcTests/MyCalcTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyСalculatio;
+
+namespace MyCalcTests
+{
+    [TestClass]
+    public class MyCalcTests
+    {
+        [TestMethod]
+        public void TestMethodTest()
+        {
+            //arrange 
+            int x = 10; int y = 20; int expected = 30;
+            //ast
+
+            MyCalc c = new MyCalc(); 
+            int actual = c.sum(x, y);
+
+            //assert
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a=1, b=4, x=3, c=2;
+            int expected = 12;
+            MyCalc m = new MyCalc();
+            int actual = m.F(a,b,x,c);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int a = 10, b = 4, x = 0, c = 2;
+            int expected = 5;
+            MyCalc m = new MyCalc();
+            int actual = m.F(a, b, x, c);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int a = 1, b = 4, x = 3, c = 0;
+            int expected = -12;
+            MyCalc m = new MyCalc();
+            int actual = m.F(a, b, x, c);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            int a = 1, b = 4, x = 0, c = 0;
+            int expected = 30;
+            MyCalc m = new MyCalc();
+            int actual = m.F(a, b, x, c);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            int a = 0, b = 4, x = 0, c = 50;
+            int expected = 30;
+            MyCalc m = new MyCalc();
+            int actual = m.F(a, b, x, c);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ MyСalculatio/MyCalc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyСalculatio
+{
+    public class MyCalc
+    {
+        public int F(int a, int b, int x, int c)
+        {
+            if (c < 0 && x != 0)
+            {
+                return -a * x - c;
+            }
+            
+            if (c > 0 && x == 0)
+            {
+                return (x - a) / -c;
+            }
+            else
+            {
+                return (b * x) / (c - a);
+            }
+
+        }
+        public int sum(int x, int y)
+        {
+
+            return x + y;
+
+        }
+      
+    }
+}
